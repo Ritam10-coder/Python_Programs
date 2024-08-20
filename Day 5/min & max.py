7c39fda433d4143478552228f895231aaa1a099b
Ritam10-coder <ritamkarmakar22@gmail.com>
2024-08-21 01:22:03 +0530

    Create min & max.py

---
+++ Day 5/min & max.py
@@ -0,0 +1,7 @@
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+ages.sort()
+min_age = ages[0]
+max_age = ages[-1]
+print("Sorted ages:", ages)
+print("Minimum age:", min_age)
+print("Maximum age:", max_age)
